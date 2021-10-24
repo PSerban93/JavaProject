a57ed0ad6f5202942f31f4f92094e171c0423944
Perseca Serban <Serby122005@yahoo.com>
2021-10-24 21:47:53 +0300

    Tema JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,58 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
+
+        System.out.println("Serban");
+
+        System.out.println("Suma a doua numere");
+
+        System.out.println(28+1993);
+
+        System.out.println("Impartirea a doua numere");
+        double nr1 = 1252;
+        double nr2 = 288;
+
+        double impartire = nr1 / nr2;
+
+        System.out.println(impartire);
+
+        System.out.println("Rezultate operatii");
+
+        System.out.println("ex_a");
+        System.out.println(-5 + 8 * 6);
+
+        System.out.println("ex_b");
+        System.out.println((55+9) % 9);
+
+        System.out.println("ex_c");
+        double ex_c1 = 20;
+        double ex_c2 = 3;
+        double ex_c3 = 5;
+        double ex_c4 = 8;
+        double rezultat = (ex_c1 + -ex_c2*ex_c3 / ex_c4);
+        System.out.println(rezultat);
+
+        System.out.println("ex_d");
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
